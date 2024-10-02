3e1d5f587e0773807f5613f7be9536fc0f3aecd8
Deezeddd <kalpved123@gmail.com>
2024-10-02 16:54:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function getCurrentYearLastDate() {
+  const currentYear = new Date().getFullYear();
+  const lastDate = new Date(currentYear, 12, 31); 
+  return lastDate;
+}
+function updateTime() {
+  const lastDate = getCurrentYearLastDate();
+  const remainingTime = lastDate - new Date();
+  let days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+  if(document != null){
+    document.getElementById(
+      "time"
+    ).innerHTML = `${days} days, ${hours} hours, ${seconds} seconds`; 
+  }
+}
+
+
+updateTime();
+setInterval(updateTime, 1000);
